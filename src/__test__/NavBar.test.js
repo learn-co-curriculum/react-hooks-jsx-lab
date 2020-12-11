564166c3c70aa38b6be1e83f1669fd3eb9993db8
ihollander <ianholla@gmail.com>
2020-12-11 09:15:37 -0500

    Updated starter code

--- src/__test__/NavBar.test.js
+++ src/__test__/NavBar.test.js
@@ -19,9 +19,9 @@ test("renders each <a> element with the correct attributes", () => {
   render(<NavBar />);
   const home = screen.queryByText(/home/i);
   expect(home).toBeInTheDocument();
-  expect(home.href).toEqual("#home");
+  expect(home.href).toContain("#home");
 
   const about = screen.queryByText(/about/i);
   expect(about).toBeInTheDocument();
-  expect(about.href).toEqual("#about");
+  expect(about.href).toContain("#about");
 });
