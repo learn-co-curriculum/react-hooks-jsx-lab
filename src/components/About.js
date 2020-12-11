6fa275ef2a4df3e8aa881bfb97c35b61395039cc
ihollander <ianholla@gmail.com>
2020-12-11 09:15:22 -0500

    Added solution

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>I Made This</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,13 @@ import React from "react";
 import { name, city } from "../data/data.js";
 
 function Home() {
-  // update the JSX being returned!
-  return <div>Home</div>;
+  return (
+    <div id="home">
+      <h1 style={{ color: "firebrick" }}>
+        {name} is a Web Developer from {city}
+      </h1>
+    </div>
+  );
 }
 
 export default Home;
