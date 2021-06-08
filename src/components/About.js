07193ee28fa1932a6eabe3e63579a3196d1eb1ba
Edward <jaysongraphics@gmail.com>
2021-06-07 20:14:13 -0400

    commit tree 578442f4a1691b8f71a66fa296a550e94ff28fa0

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id ="about">
+    <h2>About Me</h2>
+    <p>This is my first time coding in JSX!</p>
+    <img src = {image} alt="I made this"></img>
+  </div>;
 }
 
 export default About;
