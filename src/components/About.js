c49fda4092392246fbbed61bc0d2bec16abe4ba5
Hunter Kassow <hunterkassow@protonmail.com>
2022-06-06 11:13:26 -0400

    commit tree bc9a096a8db42d68b313697eff4d19195982f6ab

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+  <div id='about'>
+    <h2>About Me</h2>
+    <p>No thank you</p>
+    <img src = {image} alt='I made this'></img>
+  </div>
+  );
 }
 
 export default About;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,15 @@ import React from "react";
 
 function NavBar() {
   // update the JSX being returned!
-  return <nav>NavBar</nav>;
+  return (
+  <nav>
+    <Anchor name = 'about' title ='About'/>
+    <Anchor name = 'home' title ='Home' />
+  </nav>
+  );
+}
+function Anchor(props){
+  return <a href={'#'+props.name}>{props.title}</a>
 }
 
 export default NavBar;
