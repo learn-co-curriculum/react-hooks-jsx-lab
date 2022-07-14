9fc9751fa358063367a8767d743025de3c516520
maclaine <maclainekuria@gmail.com>
2022-07-14 12:22:40 +0300

    commit tree d9688982ab99f024e1d0adc30b2cc414dc5cd322

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,13 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return (
+    <div id="home">
+      <h1 style={{ color: "firebrick" }}>
+        {name} is a Web Developer from {city}
+      </h1>
+    </div>
+  );
 }
 
 export default Home;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,12 @@ import React from "react";
 
 function NavBar() {
   // update the JSX being returned!
-  return <nav>NavBar</nav>;
+  return (
+    <nav>
+      <a href="#home">Home</a>
+      <a href="#about">About</a>
+    </nav>
+  );
 }
 
 export default NavBar;
