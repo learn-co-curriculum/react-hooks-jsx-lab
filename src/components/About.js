d3b2c5578cad27bb59b4970bcf8f639347496e8a
lorain <laureen.akinyi@student.moringaschool.com>
2022-07-19 16:32:30 +0300

    commit tree 405150c47af5c788ff495ec8ae2472dda9eb86c6

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">About
+         <h2>About Me</h2>
+         <p>Learning JSX</p>
+         <img src={image} alt="I made this"/>
+      </div>;
 }
 
 export default About;
