8d77b2a3a6e73663bd3757b724ff3d7053d1329b
Markmutuku <mark.mutuku@student.moringaschool.com>
2023-01-12 14:31:14 +0300

    Completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
-}
+  return (
+  <div id= "about">
+    <h2>About Me</h2>
+    <p>A bonafide G and a certified gump</p>
+   <img src={image} alt="I made this"></img>
+  </div>
+    
+)}
 
 export default About;
